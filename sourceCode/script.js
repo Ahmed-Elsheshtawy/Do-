834d88fd89865b4087da9653df708ba6aa5d6e6d
Ahmed Elsheshtawy <ahmedk.elsheshtawy@gmail.com>
2025-12-12 19:21:41 +0300

    Removed some lines and cleaned some code in script.js

--- sourceCode/script.js
+++ sourceCode/script.js
@@ -8,7 +8,6 @@ class task{
     constructor(task_text, node){
         this.task_text = task_text;
         this.node = node;
-        node.querySelector('.task-text').innerText = task_text;
     }
     task_text;
     node;
@@ -18,18 +17,16 @@ let tasks = []; // list of task objects
 
 // on click 
 addTaskButton.addEventListener('click', (event)=>{
-    //if(textField.value.trim() !== ""){
+    if(textField.value.trim() !== ""){
         taskCount++;
         let task_text = textField.value;
         let task_node = taskMold.cloneNode(true);
         task_node.id = `task${taskCount}`;
-        //task_node.innerHTML = '<div class = "task-text"></div><div class = "task-options-icons-container"><img src="img/bin.png" class="task-options-icon"><img src="img/Star.png" class="task-options-icon"></div>';
+        task_node.querySelector('.task-text').innerText = task_text;
         console.log(task_node);
         tasks.push(new task(task_text, task_node)); // to store the object in an array. Not really useful now
         tasksContainer.insertBefore(task_node, taskMold);
-        //}
-
-
         textField.value = ""; // reset text field
+    }
 })
 
